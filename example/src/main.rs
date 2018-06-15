913bc5ff31e016978fe46ff88b79045cd6182d60
Sean Bruton <sean@uberfoo.net>
2018-06-14 23:43:36 -0500

    loops example app

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,13 +3,18 @@ extern crate bme280;
 
 use hal::{Delay, I2cdev};
 use bme280::BME280;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
-    let mut bme280 = BME280::new_primary(i2c_bus, Delay);
+    let mut bme280 = BME280::new_secondary(i2c_bus, Delay);
     bme280.init().unwrap();
-    let measurements = bme280.measure().unwrap();
-    println!("Relative Humidity = {}%", measurements.humidity);
-    println!("Temperature = {} deg C", measurements.temperature);
-    println!("Pressure = {} pascals", measurements.pressure);
+    loop {
+        let measurements = bme280.measure().unwrap();
+        println!("Relative Humidity = {}%", measurements.humidity);
+        println!("Temperature = {} deg C", measurements.temperature);
+        println!("Pressure = {} pascals", measurements.pressure);
+        thread::sleep(Duration::from_secs(1));
+    }
 }
